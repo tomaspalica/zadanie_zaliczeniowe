2e4e31c81547038bd2a3a9f98d0670bbb7c4b4eb
tomaspalica <tomasss.palica@gmail.com>
2025-03-01 12:40:05 +0100

    add hitory in calc and factorial

--- logic/calculator.py
+++ logic/calculator.py
@@ -1,22 +1,60 @@
+import math
+def addition():
+    try: 
+        firstNumber = float(input("type in a number"))
+        secondNumber = float(input("type in a number"))
+    except ValueError:
+        print("error type in a real number")
+        return 
+    sum = f'{firstNumber} + {secondNumber} = {firstNumber + secondNumber}'
+    return sum
+def substraction():
+    try: 
+        firstNumber = float(input("type in a number"))
+        secondNumber = float(input("type in a number"))
+    except ValueError:
+        print("error type in a real number")
+        return 
+    sum = f'{firstNumber} - {secondNumber} = {firstNumber - secondNumber}' 
+    return sum
 
-def addition(firstNumber, secondNumber):
-    sum = firstNumber + secondNumber
-    print(sum)
-def substraction(firstNumber, secondNumber):
-    sum = firstNumber - secondNumber
-    print(sum)
+def division():
+    try: 
+        firstNumber = float(input("type in a number"))
+        secondNumber = float(input("type in a number"))
+    except ValueError:
+        print("error type in a real number")
+        return 
+    sum = f'{firstNumber} / {secondNumber} = {firstNumber / secondNumber}'
+    return sum
 
-def division(firstNumber, secondNumber):
-    sum = firstNumber / secondNumber
-    print(sum)
+def multiplication():
+    try: 
+        firstNumber = float(input("type in a number"))
+        secondNumber = float(input("type in a number"))
+    except ValueError:
+        print("error type in a real number")
+        return 
+    sum = f'{firstNumber} * {secondNumber} = {firstNumber * secondNumber}' 
+    return sum
 
-def multiplication(firstNumber, secondNumber):
-    sum = firstNumber * secondNumber
-    print(sum)
-
-def exponentiation(firstNumber, secondNumber):
-    sum = firstNumber ** secondNumber
-    print(sum)
+def exponentiation():
+    try: 
+        firstNumber = float(input("type in a number"))
+        secondNumber = float(input("type in a number"))
+    except ValueError:
+        print("error type in a real number")
+        return
+    sum = f'{firstNumber} ^ {secondNumber} = {firstNumber ** secondNumber}'
+    return sum
+def factorial():
+    try:
+        onlyNumber = int(input("type in a number to use factorial on"))
+    except ValueError:
+        print("Error: type in a real number")
+        return
+    sum = f'{onlyNumber}! = {math.factorial(onlyNumber)}'
+    return sum
 def operations():
     print("""
     Calculator Operations:
@@ -25,5 +63,12 @@ def operations():
           / = divide two numbers 
           * = multiplie two numbers
           ^ = exponentiate two numbers
+          ! = get a factorial of single a number
           q = exit calculator
     """)
+def history(result):
+    with open("history.txt","a") as file:
+        file.write(result + '\n')
+        print(result)
+        print("Data was saved in file history.txt")
+        file.close()
--- modes/calculator.py
+++ modes/calculator.py
@@ -2,33 +2,35 @@ import logic.calculator
 def calculator():
     while True:
         logic.calculator.operations()
-        operation = input("Calculator, type operation: ")
+        operation = input("Calculator, type operation: ").strip()
         if(operation == "q"):
             break
-        if(operation in ('+','-','*','/',"^")):
-            try:
-                firstNumber = float(input("type in a number"))
-                secondNumber = float(input("type in a number"))
-            except ValueError:
-                print("error type in a real number")
-                continue
-            match operation:
-                case "+":
-                    logic.calculator.addition(firstNumber,secondNumber)
-                case "-":
-                    logic.calculator.substraction(firstNumber,secondNumber)
-                case "/":
-                    try:
-                        logic.calculator.division(firstNumber,secondNumber)
-                    except ZeroDivisionError:
-                        print("Error you can't divide by zero")
-                        continue
-                case "*":
-                    logic.calculator.multiplication(firstNumber,secondNumber)
-                case "^":
-                    logic.calculator.exponentiation(firstNumber,secondNumber)
-        else:
-            print('This command dosnt exist, chose one from the list below')
+    
+        match operation:
+            case "+":
+                result = logic.calculator.addition()
+                logic.calculator.history(result)
+            case "-":
+                result = logic.calculator.substraction()
+                logic.calculator.history(result)
+            case "/":
+                try:
+                    result = logic.calculator.division()
+                    logic.calculator.history(result)
+                except ZeroDivisionError:
+                    print("Error you can't divide by zero")
+                    continue
+            case "*":
+                result = logic.calculator.multiplication()
+                logic.calculator.history(result)
+            case "^":
+                result = logic.calculator.exponentiation()
+                logic.calculator.history(result)
+            case "!":
+                result = logic.calculator.factorial()
+                logic.calculator.history(result)
+            case _:
+                print('This command dose not exist, chose one from the list below')
 
     
     
